5096611ba48ffa1b305511cbd4c8404a6ed2a388
Sérgio Damo de Lemos <sergio@42vivid.com>
2019-11-20 22:46:07 -0800

    Finished chapter 1

--- src/github.com/sergiodl/interpreter/token/token.go
+++ src/github.com/sergiodl/interpreter/token/token.go
@@ -18,6 +18,13 @@ const (
 	// Operators
 	ASSIGN = "="
 	PLUS   = "+"
+	MINUS  = "-"
+	BANG   = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
@@ -28,14 +35,27 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	EQ     = "=="
+	NOT_EQ = "!=" 
+
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn"     : FUNCTION,
+	"let"    : LET,
+	"true"   : TRUE,
+	"false"  : FALSE,
+	"if"     : IF,
+	"else"   : ELSE,
+	"return" : RETURN,
 }
 
 func LookupIdentifier(ident string) TokenType {
